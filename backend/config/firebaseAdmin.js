414057acbe24e63991fa28379479cd4d467881f2
Hitarth Pareek <Hitarth.pareek@akar.local>
2026-07-21 12:50:43 +0530

    before refactoring middlewares backend api's for superadmin working

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,9 +1,18 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 
-const serviceAccount = require("./firebaseServiceAccount.json");
-
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
 });
 
 console.log("✅ Firebase Admin Initialized");
